c10a4b95905a23e7d9eca0497276903e0c898828
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-12-11 12:29:43 +0000

    function added to module

--- livingOrganism/animal_types/mammals.rb
+++ livingOrganism/animal_types/mammals.rb
@@ -1,4 +1,17 @@
 module Mammals
-  
+  def self.common_mammal_traits
+    puts 'Mammals are warm blooded and have vertebrae'
+  end
+
+  class CommonTraits
+
+    def feed_young
+      puts 'all mammals feed thier young with milk'
+    end
+
+  end
 
 end
+
+dolphin = Mammals::CommonTraits.new
+dolphin.feed_young
